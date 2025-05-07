2244ff09940d47a50f9b4157e05069d210f3bfd5
Sasank5716 <sasanklama5716@gmail.com>
2025-05-07 10:39:41 +0545

    Used IANA time zone name

--- Digital_clock/digital_clock.js
+++ Digital_clock/digital_clock.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('clock');
 const ctx = canvas.getContext('2d');
 
 const countryLabel = "Nepal (Kathmandu)";
-const timeZone = "Nepal Standard Time (GMT+5:45)"; 
+const timeZone = "Asia/Kathmandu"; 
 
 function drawClock() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -39,7 +39,21 @@ function drawClock() {
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillStyle = "#00FF99";
-    ctx.fillText(timeString, canvas.width / 2, canvas.height / 2);
+    ctx.fillText(timeOnly, canvas.width / 2, canvas.height / 2 - 20);
+
+    ctx.font = "bold 28px monospace";
+    ctx.fillStyle = "#FFD700";
+    ctx.fillText(tzAbbr ? tzAbbr[1] : "", canvas.width / 2, canvas.height / 2 + 40);
+  
+    // Draw date
+    ctx.font = "20px monospace";
+    ctx.fillStyle = "#fff";
+    ctx.fillText(dateString, canvas.width / 2, canvas.height - 40);
+  
+    // Draw country label
+    ctx.font = "18px monospace";
+    ctx.fillStyle = "#aaa";
+    ctx.fillText(countryLabel, canvas.width / 2, 30);
   }
 
 setInterval(drawClock, 1000);
