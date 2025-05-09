b41f040ac2f702e9a0572fab99283fa542bf3fb5
Sasank5716 <sasanklama5716@gmail.com>
2025-05-09 09:03:28 +0545

    Add Invert Filter

--- Image_editor/Image_editor.js
+++ Image_editor/Image_editor.js
@@ -33,3 +33,15 @@ function applyGrayscale() {
     }
     ctx.putImageData(imgData, 0, 0);
   }
+
+  // Invert filter
+function applyInvert() {
+  if (!imgData) return;
+  let data = imgData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i];       // R
+    data[i + 1] = 255 - data[i+1]; // G
+    data[i + 2] = 255 - data[i+2]; // B
+  }
+  ctx.putImageData(imgData, 0, 0);
+}
