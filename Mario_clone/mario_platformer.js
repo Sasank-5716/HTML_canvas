bb599354ce8cc4224553e1aad71268a639990662
Sasank5716 <sasanklama5716@gmail.com>
2025-05-17 09:21:36 +0545

    Define keys and player class

--- Mario_clone/mario_platformer.js
+++ Mario_clone/mario_platformer.js
@@ -12,3 +12,20 @@ const GRAVITY = 0.6;
 const FRICTION = 0.8;
 const PLAYER_SPEED = 7;  // slightly faster for bigger canvas
 const JUMP_POWER = 18;   // stronger jump for bigger canvas
+
+let keys = {
+  left: false,
+  right: false,
+  up: false,
+};
+
+class Player {
+  constructor() {
+    this.width = 50;
+    this.height = 50;
+    this.x = 100;
+    this.y = 0;
+    this.velX = 0;
+    this.velY = 0;
+    this.jumping = false;
+  }
