9ed68add65047c9b57480a7e3d000aaf01879585
Sasank5716 <sasanklama5716@gmail.com>
2025-05-26 10:48:22 +0545

    Draw paddle with rounded edges

--- Arkanoid/arkanoid.js
+++ Arkanoid/arkanoid.js
@@ -62,3 +62,26 @@ function initBricks() {
     }
   }
 }
+
+function drawPaddle() {
+  ctx.fillStyle = paddle.color;
+  ctx.beginPath();
+  // Rounded rectangle paddle
+  const x = paddle.x;
+  const y = paddle.y;
+  const w = paddle.width;
+  const h = paddle.height;
+  const r = paddle.radius;
+
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+  ctx.lineTo(x + r, y + h);
+  ctx.quadraticCurveTo(x, y + h, x, y + h - r);
+  ctx.lineTo(x, y + r);
+  ctx.quadraticCurveTo(x, y, x + r, y);
+  ctx.closePath();
+  ctx.fill();
+}
