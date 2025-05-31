9016ee63a10fd602f28159f9d4264ddecb70ba74
Sasank5716 <sasanklama5716@gmail.com>
2025-05-31 11:15:14 +0545

    Add gravity directions

--- Gravity_puzzle_platformer/puzzle_platformer.js
+++ Gravity_puzzle_platformer/puzzle_platformer.js
@@ -4,3 +4,11 @@ const ctx = canvas.getContext('2d');
 const GRAVITY_MAGNITUDE = 0.5;
 const PLAYER_SIZE = 40;
 const TILE_SIZE = 50;
+
+// Gravity directions
+const GRAVITY_DIRECTIONS = {
+  DOWN: { x: 0, y: 1 },
+  UP: { x: 0, y: -1 },
+  LEFT: { x: -1, y: 0 },
+  RIGHT: { x: 1, y: 0 },
+};
